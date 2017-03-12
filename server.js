b3974a66ada71ebbb299257b1812318900620977
Andrew Beers <andybeers@gmail.com>
2017-03-11 19:30:54 -0800

    eslint setting for es8 FUTURETIME

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const Koa = require('koa');
 const app = new Koa();
 const morgan = require('koa-morgan');
 
+app.use(async function (ctx, next) {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
 app.use(morgan('dev'));
 
 app.use(ctx => {
