e4f88a5cb62c28588b96cc334eb786f5cba26895
Andrew Beers <andybeers@gmail.com>
2017-03-12 21:39:32 -0700

    Added bodyparser, createserver callback, and basic routes

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
 const app = require('./lib/app');
-// require('./lib/mongoose-setup');
+require('./lib/setup-mongoose');
 
 const server = http.createServer(app.callback());
 server.listen(port, () => {
   console.log('server currently connected to port', port);
 });
+
+//what's the purpose of invoking
+//createServer with app.callback()????
